2da0f91d0d68236f06bbbd2699b88585a8c5454c
Adeola Odeyemi <dbrizomomi@icloud.com>
2019-11-25 03:06:11 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,4 +1,4 @@
 {mario} = "It's-a me, Mario!\n"
 {toadstool} = "Thank You Mario! But Our Princess Is In Another Castle!\n"
 {link} = "It's Dangerous To Go Alone! Take This.\n"
-
+{any_phrase} = "Do A Barrel Roll!\n"
